e9d02173ccf91055968acb5d857f96f1f8fdaacd
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-25 23:10:39 +0100

    adds closure exercises

--- ch5/2-closures/main.go
+++ ch5/2-closures/main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
-	// todo
+
+	// ### - closure as parameter
+	type Person struct {
+		FirstName string
+		LastName  string
+		Age       int
+	}
+
+	people := []Person{
+		{"Ken", "Thompson", 88},
+		{"Dennis", "Ritchie", 0},
+		{"Rob", "Pike", 60},
+	}
+
+	fmt.Println(people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+	fmt.Println(people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println(people)
+	fmt.Println("")
+
+	// ### - closure as return value
+	twoBase := makeMult(2)
+	threeBase := makeMult(3)
+	for i := 0; i < 3; i++ {
+		fmt.Println(twoBase(i), threeBase(i))
+	}
+}
+
+func makeMult(base int) func(int) int {
+	return func(factor int) int {
+		return base * factor
+	}
 }
