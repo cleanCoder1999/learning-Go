c371d077bd8b75456c00c028637fe4ffa9c33d34
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-25 19:22:44 +0100

    showcases shadowing of predeclared identifiers

--- ch4/1-shadowing/main.go
+++ ch4/1-shadowing/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func main() {
-	// ###
+	fmt.Println(true)
+
+	// Go contains only 25 keywords;
+	// predeclared identifiers such as int, string, bool, true or false are not part of those 25 keywords
+	// those are part of Go's "universal block"
+	// the universal block is the block that contains all other blocks of a Go program
+	true := "shadowing bool's 'true'"
+	fmt.Println(true)
+
+	var b bool
+	fmt.Println("is of type:", reflect.TypeOf(b))
+
+	bool := "now, bool is a string lol"
+	fmt.Println(bool)
 }
