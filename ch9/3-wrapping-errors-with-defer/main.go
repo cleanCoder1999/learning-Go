b9b11a70bf978a9316f303104b35f060e78def75
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-31 10:32:56 +0100

    implements some examples on how to wrap errors with defer

--- ch9/3-wrapping-errors-with-defer/main.go
+++ ch9/3-wrapping-errors-with-defer/main.go
@@ -1,5 +1,60 @@
 package main
 
+import "fmt"
+
 func main() {
 
+}
+
+func DoSomethingWithDefer(val1 int, val2 string) (_ string, err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("in DoSomethingWithDefer: %w", err)
+		}
+	}()
+
+	val3, err := doThing1(val1)
+	if err != nil {
+		return "", err
+	}
+
+	val4, err := doThing2(val2)
+	if err != nil {
+		return "", err
+	}
+
+	return doThing3(val3, val4)
+}
+
+
+// do thing w/o defer
+func DoSomething(val1 int, val2 string) (string, error) {
+	val3, err := doThing1(val1)
+	if err != nil {
+		return "", fmt.Errorf("in DoSomething: %w", err)
+	}
+
+	val4, err := doThing2(val2)
+	if err != nil {
+		return "", fmt.Errorf("in DoSomething: %w", err)
+	}
+
+	result, err := doThing3(val3, val4)
+	if err != nil {
+		return "", fmt.Errorf("in DoSomething: %w", err)
+	}
+
+	return result, nil
+}
+
+func doThing1(val1 int) (int, error) {
+	return 0, nil
+}
+
+func doThing2(val2 string) (string, error) {
+	return "", nil
+}
+
+func doThing3(val3 int, val4 string) (string, error) {
+	return "", nil
 }
