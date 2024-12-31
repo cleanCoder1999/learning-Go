28a373b0d2146b505c2d7b32f52f0357f7af6da1
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-31 10:42:26 +0100

    implements an example on how to recover from a panic

--- ch9/4-panic-recover/main.go
+++ ch9/4-panic-recover/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for _, val := range []int{1, 2, 0, 6} {
+		div60(val)
+	}
+}
+
+func div60(i int) {
+
+	// NOTE: recover() must be called from within a defer because once a panic happens, only deferred functions are run
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Println(v)
+		}
+	}()
+
+	fmt.Println(60 / i)
 }
