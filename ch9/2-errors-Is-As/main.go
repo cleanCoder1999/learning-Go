1bb060f0d10899ad960ab5cfd19dbbd9165a1aad
Lucas Müllner <lucasmuellner@gmail.com>
2024-12-31 10:13:07 +0100

    implements some examples on how to use errors.Is / .As

--- ch9/2-errors-Is-As/main.go
+++ ch9/2-errors-Is-As/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
+type MyErr struct {
+	Codes []int
+}
+
+func main() {
+	// ### errors.Is()
+	{
+		err := fileChecker("test.txt")
+		if err != nil {
+
+			// errors.Is() returns true if any error in the error tree matches the Sentinel error os.ErrNotExist
+			if errors.Is(err, os.ErrNotExist) {
+				fmt.Println("file does not exist")
+			}
+		}
+	}
+
+	// ### errors.As()
+	{
+		err := funcThatReturnsAnError()
+		var myErr MyErr
+		if errors.As(err, &myErr) {
+			fmt.Println(myErr.Codes)
+		}
+	}
+}
+
+func funcThatReturnsAnError() error {
+	return errors.New("something bad happened")
+}
+
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in fileChecker: %w", err)
+	}
+	f.Close()
+	return nil
 }
